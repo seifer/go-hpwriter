a392e8fafc2d788a02f5d12101f5104b9659bb52
Nurutdinov Timur Ayratovich <timur.nurutdinov@lamoda.ru>
2017-04-14 12:29:09 +0300

    /dev/null

--- bench_test.go
+++ bench_test.go
@@ -24,11 +24,43 @@ func (w *Locked) Write(buf []byte) (n int, err error) {
 	return
 }
 
-var w = os.Stderr
 var m = []byte("test\n")
-var wLock = NewLocked(w)
-var wChannel = NewThroughChannel(w)
-var wShardedBuffers = NewThroughShardedBuffers(w, 32)
+var w, wLock, wChannel, wShardedBuffers io.Writer
+
+func init() {
+	var err1 error
+
+	w, err1 = os.OpenFile("/dev/null", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	file1, err := os.OpenFile("/dev/null", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	wLock = NewLocked(file1)
+
+	file2, err := os.OpenFile("/dev/null", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	wChannel = NewThroughChannel(file2)
+
+	file3, err := os.OpenFile("/dev/null", os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0755)
+	if err != nil {
+		panic(err)
+	}
+	wShardedBuffers = NewThroughShardedBuffers(file3, 32)
+}
+
+func BenchmarkFree(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			w.Write(m)
+		}
+	})
+}
 
 func BenchmarkLock(b *testing.B) {
 	b.RunParallel(func(pb *testing.PB) {
